918341e2d12b3816ba6fc9c3bc0e371d347c71fb
Abai <abai2010@bk.ru>
2017-12-27 23:25:17 +0600

    finished

---
+++ pract5/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace pract5
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                TestMethod();
+            }
+            
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+
+            Console.ReadLine();
+        }
+
+        static void TestMethod()
+        {
+            int x = 4;
+            int y = 0;
+            int result = x / y;
+
+            Console.WriteLine("Успешно!");
+        }
+    }
+}
